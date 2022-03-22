d796d98fce3aed60fbcc6f8b6d34156190b4c749
brendongama <brendon_gama@hotmail.com>
2022-03-21 21:13:25 -0300

    Propriedade nullable de @Column e @JoinColumn

--- src/main/java/com/brendon/algafood/domain/model/Restaurante.java
+++ src/main/java/com/brendon/algafood/domain/model/Restaurante.java
@@ -2,6 +2,7 @@ package com.brendon.algafood.domain.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,7 +23,10 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false)
 	private String nome;
+	
+	@Column(nullable = false)
 	private BigDecimal taxaFrete;
 	
 	@ManyToOne
