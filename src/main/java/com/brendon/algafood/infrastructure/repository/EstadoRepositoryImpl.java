00e345c30756a32b10380a3a6ae52c980a8f5416
brendongama <brendon_gama@hotmail.com>
2022-03-22 09:46:32 -0300

    Implementando os repositórios

---
+++ src/main/java/com/brendon/algafood/infrastructure/repository/EstadoRepositoryImpl.java
@@ -0,0 +1,41 @@
+package com.brendon.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.brendon.algafood.domain.model.Estado;
+import com.brendon.algafood.domain.repository.EstadoRepository;
+
+public class EstadoRepositoryImpl implements EstadoRepository{
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Estado> listar() {
+		return manager.createQuery("from Estado", Estado.class).getResultList();
+	}
+
+	@Override
+	public Estado buscar(Long id) {
+		return manager.find(Estado.class, id);
+	}
+
+	@Transactional
+	@Override
+	public Estado salvar(Estado estado) {
+		return manager.merge(estado);
+	}
+
+	@Transactional
+	@Override
+	public void remover(Estado estado) {
+		estado = buscar(estado.getId());
+		manager.remove(estado);		
+	}
+	
+	
+}
---
+++ src/main/java/com/brendon/algafood/infrastructure/repository/FormaPagamentoRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.brendon.algafood.infrastructure.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.brendon.algafood.domain.model.FormaPagamento;
+import com.brendon.algafood.domain.repository.FormaPagamentoRepository;
+
+public class FormaPagamentoRepositoryImpl implements FormaPagamentoRepository{
+
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<FormaPagamento> listar() {
+		return manager.createQuery("from FormaPgamaneto", FormaPagamento.class).getResultList();
+	}
+
+	@Override
+	public FormaPagamento buscar(Long id) {
+		return manager.find(FormaPagamento.class, id);
+	}
+
+	@Transactional
+	@Override
+	public FormaPagamento salvar(FormaPagamento formaPagamento) {
+		return manager.merge(formaPagamento);
+	}
+
+	@Transactional
+	@Override
+	public void remover(FormaPagamento formaPagamento) {
+		formaPagamento = buscar(formaPagamento.getId());
+		manager.remove(formaPagamento);		
+	}
+}
