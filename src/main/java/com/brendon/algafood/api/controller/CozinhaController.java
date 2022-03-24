15413e9694a5988b99f0793be52df6cd5fce336e
brendongama <brendon_gama@hotmail.com>
2022-03-24 15:29:42 -0300

    Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/brendon/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/brendon/algafood/api/controller/CozinhaController.java
@@ -38,10 +38,11 @@ public class CozinhaController {
 	@GetMapping("/{cozinhaId}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha =  cozinhaRepository.buscar(cozinhaId);		
-		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-		//return ResponseEntity.ok(cozinha);
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.LOCATION, "Https://localhost:8080/cozinhas");
-		return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
+		if(cozinha != null) {
+			return ResponseEntity.ok(cozinha);
+		}
+		
+		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		return ResponseEntity.notFound().build();
 	}
 }
