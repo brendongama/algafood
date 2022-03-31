ada6d8d3796f8ec7aef2cb73431210748b43a2b0
brendongama <brendon_gama@hotmail.com>
2022-03-30 22:24:34 -0300

    Analisando solução para atualização parcial de recursos com PATCH

--- src/main/java/com/brendon/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/brendon/algafood/api/controller/RestauranteController.java
@@ -1,11 +1,13 @@
 package com.brendon.algafood.api.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -65,8 +67,22 @@ public class RestauranteController {
 			return ResponseEntity.notFound().build();
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
+		}		
+	}
+	
+	@PatchMapping("/{restauranteId}")
+	public ResponseEntity<?> alterarParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos) {
+		Restaurante restauranteAtual = restauranteService.buscar(restauranteId);
+		if(restauranteAtual == null) {
+			return ResponseEntity.notFound().build();
 		}
 		
-		
+		return alterar(restauranteId, restauranteAtual);
+	}
+	
+	private void merge(Map<String, Object> camposOrigem, Restaurante restauranteDestino) {
+		camposOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+			
+		});
 	}
 }
