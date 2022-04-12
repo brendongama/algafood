3a0c647efceec66f3432092db56c88c0155fe248
brendongama <brendon_gama@hotmail.com>
2022-04-11 21:57:12 -0300

    Adicionando restrições na cláusula where com Criteria API

--- src/main/java/com/brendon/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/brendon/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -7,6 +7,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -24,7 +26,15 @@ public class RestauranteRepositoryImpl {
 		
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
 	
-		criteria.from(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		Predicate nomePredicate = builder.like(root.get("nome"),"%" +  nome + "%");
+		
+		Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 		
 		return manager.createQuery(criteria).getResultList();
 	}
