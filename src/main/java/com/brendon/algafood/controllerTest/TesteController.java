00da7b65a04f113ae8c7f9250c003a3e10b85f8c
brendongama <brendon_gama@hotmail.com>
2022-04-13 11:37:47 -0300

    Estendendo o JpaRepository para customizar o repositório base

--- src/main/java/com/brendon/algafood/controllerTest/TesteController.java
+++ src/main/java/com/brendon/algafood/controllerTest/TesteController.java
@@ -2,13 +2,16 @@ package com.brendon.algafood.controllerTest;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.brendon.algafood.domain.model.Cozinha;
 import com.brendon.algafood.domain.model.Restaurante;
+import com.brendon.algafood.domain.repository.CozinhaRepository;
 import com.brendon.algafood.domain.repository.RestauranteRepository;
 import com.brendon.algafood.infrastructure.repository.spec.RestauranteSpecs;
 
@@ -19,6 +22,9 @@ public class TesteController {
 	
 	@Autowired 
 	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@GetMapping("/restaurante/por-taxa-frete")
 	public List<Restaurante> restaurantesPorTaxaFretes(BigDecimal taxaInicial, BigDecimal taxaFinal){
@@ -35,6 +41,16 @@ public class TesteController {
 		return restauranteRepository.findAll(
 				RestauranteSpecs.comFreteGratis().and(
 						RestauranteSpecs.comNomeSemelhante(nome)));
+	}
+	
+	@GetMapping("/cozinha/primeira")
+	public Optional<Cozinha> cozinhaPrimeira(){
+		return cozinhaRepository.buscarPrimeiro();
+	}
+	
+	@GetMapping("/restaurante/primeiro")
+	public Optional<Restaurante> restaurantePrimeiro(){
+		return restauranteRepository.buscarPrimeiro();
 	}
 	
 
