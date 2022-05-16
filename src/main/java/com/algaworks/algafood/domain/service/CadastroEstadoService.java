e9917a737196a2997092214fedaac56a47a42da3
brendongama <brendon_gama@hotmail.com>
2022-05-16 20:17:21 -0300

    Afinando a granularidade e definindo a hierarquia das exceptions de negócios

--- src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -5,8 +5,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
-import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
-import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EstadoNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Estado;
 import com.algaworks.algafood.domain.repository.EstadoRepository;
 
@@ -14,7 +13,6 @@ import com.algaworks.algafood.domain.repository.EstadoRepository;
 public class CadastroEstadoService {
 
 	private static final String MSG_ESTADO_NAO_PODE_SER_REMOVIDO = "Estado de código %d não pode ser removido, pois está em uso";
-	private static final String MSG_NAO_EXISTE_CADASTRO = "Não existe um cadastro de estado com código %d";
 
 	@Autowired
 	private EstadoRepository estadoRepository;
@@ -28,16 +26,16 @@ public class CadastroEstadoService {
 			estadoRepository.deleteById(estadoId);
 
 		} catch (EmptyResultDataAccessException e) {
-			throw new EntidadeNaoEncontradaException(String.format(MSG_NAO_EXISTE_CADASTRO, estadoId));
+			throw new EstadoNaoEncontradaException(estadoId);
 
 		} catch (DataIntegrityViolationException e) {
-			throw new EntidadeEmUsoException(String.format(MSG_ESTADO_NAO_PODE_SER_REMOVIDO, estadoId));
+			throw new EstadoNaoEncontradaException(String.format(MSG_ESTADO_NAO_PODE_SER_REMOVIDO, estadoId));
 		}
 	}
 
 	public Estado buscarOuFalhar(Long estadoId) {
 		return estadoRepository.findById(estadoId).orElseThrow(
-				() -> new EntidadeNaoEncontradaException(String.format(MSG_NAO_EXISTE_CADASTRO, estadoId)));
+				() -> new EstadoNaoEncontradaException(estadoId));
 	}
 
 }
