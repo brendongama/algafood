d28e23270407095bff9361dc06d61c8bb01f97a4
brendongama <brendon_gama@hotmail.com>
2022-06-01 18:05:44 -0300

    Agrupando e restringindo constraints que devem ser usadas na validação

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -9,8 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+import com.algaworks.algafood.Groups;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
@@ -22,11 +24,12 @@ import lombok.EqualsAndHashCode;
 public class Cozinha {
 
 	@EqualsAndHashCode.Include
-	@NotNull
+	@NotNull(groups = Groups.CadastroRestaurante.class)
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
